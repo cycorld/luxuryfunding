196a16c9d184c83ef8ac43c55b0941eb29b69d2d
cycorld <charleschoi87@gmail.com>
2015-08-27 23:32:08 +0900

    migrate default and null option added

--- db/migrate/20150827140153_create_boxes.rb
+++ db/migrate/20150827140153_create_boxes.rb
@@ -1,8 +1,8 @@
 class CreateBoxes < ActiveRecord::Migration
   def change
     create_table :boxes do |t|
-      t.integer :user_id
-      t.string :title
+      t.integer :user_id, null: false
+      t.string :title, null: false
 
       t.timestamps null: false
     end
--- db/migrate/20150827140321_create_cards.rb
+++ db/migrate/20150827140321_create_cards.rb
@@ -1,10 +1,10 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-      t.integer :box_id
-      t.string :question
-      t.string :answer
-      t.integer :stage
+      t.integer :box_id, null: false
+      t.string :question, null: false
+      t.string :answer, null: false
+      t.integer :stage, default: 1
 
       t.timestamps null: false
     end

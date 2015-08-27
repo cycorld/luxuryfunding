c4937b1d01116d8e412ac2dc09962a11c966a6c9
cycorld <charleschoi87@gmail.com>
2015-08-27 23:02:08 +0900

    rails g model box

---
+++ db/migrate/20150827140153_create_boxes.rb
@@ -0,0 +1,10 @@
+class CreateBoxes < ActiveRecord::Migration
+  def change
+    create_table :boxes do |t|
+      t.integer :user_id
+      t.string :title
+
+      t.timestamps null: false
+    end
+  end
+end

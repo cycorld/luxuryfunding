278986cfafa78faf174fdbc38d445afd1342d34c
cycorld <charleschoi87@gmail.com>
2015-08-27 23:36:24 +0900

    study controller

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -1,4 +1,11 @@
 class StudyController < ApplicationController
   def index
+    @boxes = Box.all
+  end
+
+  def study
+    b = Box.find(params[:box_id])
+    @cards = b.cards
+    @card = @cards.where(stage: params[:stage] || 1).order('updated_at DESC').first
   end
 end

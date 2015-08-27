681f5b12b1640915d8b49396ef2f681480700f02
cycorld <charleschoi87@gmail.com>
2015-08-27 23:35:46 +0900

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'study/index'
+  get '/study/:box_id' => 'study#study'
 
   root 'study#index'
 

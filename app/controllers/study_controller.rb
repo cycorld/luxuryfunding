58d85b8a69ca611980a581404218d92b550c8dfd
cycorld <charleschoi87@gmail.com>
2015-08-27 23:59:56 +0900

    stable stage

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -6,6 +6,17 @@ class StudyController < ApplicationController
   def study
     b = Box.find(params[:box_id])
     @cards = b.cards
-    @card = @cards.where(stage: params[:stage] || 1).order('updated_at DESC').first
+    if @cards.empty?
+      render text: "empty cards..."
+    else
+      @stage = params[:stage].to_i || @cards.order('updated_at DESC').last.stage
+      @stageds = @cards.where(stage: @stage)
+      if @stageds.empty?
+        @card = @cards.order('updated_at DESC').last
+        @stage = @card.stage
+      else
+        @card = @stageds.order('updated_at DESC').first
+      end
+    end
   end
 end

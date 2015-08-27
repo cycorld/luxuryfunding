6a627d70196b99eeb3daa522852c8e693d3a0927
cycorld <charleschoi87@gmail.com>
2015-08-27 23:32:23 +0900

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+user = User.create(email: "choi@likelion.net", password: "12341234", password_confirmation: "12341234")
+box = Box.create(user_id: user.id, title: "MD Vocabulary 33000")
+Card.create(box_id: box.id, question: "abdicate (from) the throne", answer: "왕좌에서 물러나다")

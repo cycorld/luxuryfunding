b2a7de10c397ccce8c5b72b6fe43c5cb5967d4e8
cycorld <charleschoi87@gmail.com>
2015-08-28 23:39:21 +0900

    day2 data

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,4 +1,10 @@
 class Box < ActiveRecord::Base
   belongs_to :user
   has_many :cards
+
+  def import file
+    CSV.foreach(file, {headers: true}) do |r|
+      Card.create(box_id: id, question: r["question"], answer: r["answer"])
+    end
+  end
 end

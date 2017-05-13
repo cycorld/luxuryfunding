2a93dc459ebcac84d4504a9064569df0c7c8a1bf
cycorld <cycorld.com@gmail.com>
2017-05-13 22:18:58 +0900

    새 박스 만들기

--- app/controllers/box_controller.rb
+++ app/controllers/box_controller.rb
@@ -9,32 +9,46 @@ class BoxController < ApplicationController
   def add_chapter
     chapter = Chapter.find(params[:id])
     return redirect_to :root if chapter.user != current_user
+    if params[:box_title].present?
+      box = Box.new(user: current_user, title: params[:box_title])
+      box.save
+      box_id = box.id
+    else
+      box_id = params[:box_id]
+    end
     total_count = chapter.cards.count
     count = 0
     chapter.cards.each do |c|
-      unless Memory.where(box_id: params[:box_id], card_id: c.id).take or !current_user.boxes.where(id: params[:box_id]).take
-        m = Memory.create(box_id: params[:box_id],
-                      card_id: c.id,
-                      question: c.question,
-                      answer: c.answer,
-                      q_lang: c.q_lang,
-                      a_lang: c.a_lang)
-        count += 1 if m
+      unless Memory.where(box_id: box_id, card_id: c.id).take or !current_user.boxes.where(id: box_id).take
+        count += 1 if Memory.create(box_id: box_id,
+                                    card_id: c.id,
+                                    question: c.question,
+                                    answer: c.answer,
+                                    q_lang: c.q_lang,
+                                    a_lang: c.a_lang
+                                   )
       end
     end
     flash[:alert] = "#{total_count} 개의 카드 중 #{count} 개의 카드를 추가했습니다!"
-    redirect_to "/study/#{params[:box_id]}"
+    redirect_to "/study/#{box_id}"
   end
 
   def add_card
     card = Card.find(params[:id])
     return redirect_to :root if card.user != current_user
-    if Memory.where(box_id: params[:box_id], card_id: params[:id]).take or !current_user.boxes.where(id: params[:box_id]).take
+    if params[:box_title].present?
+      box = Box.new(user: current_user, title: params[:box_title])
+      box.save
+      box_id = box.id
+    else
+      box_id = params[:box_id]
+    end
+    if Memory.where(box_id: box_id, card_id: params[:id]).take or !current_user.boxes.where(id: box_id).take
       flash[:alert] = "이 박스에는 이미 추가된 카드입니다."
       redirect_to :root
       return
     end
-    m = Memory.create(box_id: params[:box_id],
+    m = Memory.create(box_id: box_id,
                       card_id: params[:id],
                       question: card.question,
                       answer: card.answer,
@@ -45,6 +59,6 @@ class BoxController < ApplicationController
     else
       flash[:alert] = "카드 추가에 실패했습니다 ㅠ"
     end
-    redirect_to "/study/#{params[:box_id]}"
+    redirect_to "/study/#{box_id}"
   end
 end
